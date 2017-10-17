26692c0e2d11dc7de513c19acbc6cec6f529300e
butuhfingers <crewderek@yahoo.com>
2017-10-16 19:06:53 -0600

    Camera Setup Test Branch

---
+++ Assets/Game Assets/Player/Camera Setups/BaseCameraSetup.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseCameraSetup : MonoBehaviour, ICameraSetup {
+	//Constructor
+	public BaseCameraSetup(float x, float y, float z){
+		startCameraPosition = new Vector3(x, y, z);
+	}
+	public BaseCameraSetup() : this(0, 0, 0){}	//Call the constructor the sets the x, y, z coordinates
+
+	//Variables
+	protected Vector3 startCameraPosition;
+
+	//Properties
+	public float X{
+		get{return startCameraPosition.x;}
+	}
+	public float Y{
+		get{return startCameraPosition.y;}
+	}
+
+	public float Z{
+		get{return startCameraPosition.z;}
+	}
+}
---
+++ Assets/Game Assets/Player/Camera Setups/MurderCameraSetup.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MurderCameraSetup : BaseCameraSetup, ICameraSetup{
+	//Constructor
+	public MurderCameraSetup() : base(0, 0.5f, 0){}	//This is the relative position of the camera start position
+}
